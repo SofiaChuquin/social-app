00125ff960fdb460d44e154138b4a384d719e500
sofiachuquin <sofiachuquin@gmail.com>
2018-09-13 20:15:38 -0500

    Add action and reducer for register

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:9000/api/'
+});
 
 export function loginUser(email, password) {
-  const instance = axios.create({
-    baseURL: 'http://localhost:9000/api/'
-  });
-
   const data = {
     email,
     password,
@@ -17,6 +18,20 @@ export function loginUser(email, password) {
 
   return {
     type: LOGIN,
+    payload: request,
+  }
+}
+
+export function createUser(values) {
+  const data = {
+    values
+  }
+
+  const request = instance.post('users/sign_up', data)
+    .catch(error => error.response);
+
+  return {
+    type: REGISTER,
     payload: request,
   }
 }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,8 +1,10 @@
-import { LOGIN } from '../actions';
+import { LOGIN, REGISTER } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN:
+      return action.payload.data;
+    case REGISTER:
       return action.payload.data;
     default:
       return state;
