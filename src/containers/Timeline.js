d98ef48061c3635b02a2a700bfe7cf2e3c519096
sofiachuquin <sofiachuquin@gmail.com>
2018-09-13 22:30:04 -0500

    Log out and redirect to log in

--- src/containers/Timeline.js
+++ src/containers/Timeline.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Timeline extends Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      authorization_token: PropTypes.string,
+    }).isRequired,
+  };
+
+  logOut = () => {
+    localStorage.removeItem('authorization_token');
+  }
+
   render() {
+    if (!(localStorage.getItem('authorization_token'))) {
+      return <Redirect to='/log_in' />
+    }
     return (
       <div>
         Este es el timeline
-        <Link to="/log_in">
-          <button>Cerrar sesión</button>
+        <Link to="/log_in" onClick={this.logOut}>
+          Cerrar sesión
         </Link>
       </div>
     );
   }
 }
 
-export default Timeline;
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+
+export default connect(mapStateToProps, {})(Timeline);
